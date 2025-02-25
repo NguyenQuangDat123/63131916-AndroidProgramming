eae66799b2abce4744bb3b8effb7f3dbcb11419f
NguyenQuangDat123 <dat.nq.63cntt@ntu.edu.vn>
2025-02-25 11:08:09 +0700

    Cải tiến, giao tiếp với người dùng
    Đưa ra lời khuyên với tình trạng sức khỏe của người dùng

--- AppBMI/app/src/main/java/com/example/appbmi/MainActivity.java
+++ AppBMI/app/src/main/java/com/example/appbmi/MainActivity.java
@@ -5,6 +5,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,24 +39,32 @@ public class MainActivity extends AppCompatActivity {
 
         if (heightStr.isEmpty() || weightStr.isEmpty()) {
             tvResult.setText("0.0");
-            tvStatus.setText("Enter values");
+            tvStatus.setText("Vui lòng nhập đầy đủ thông tin");
             return;
         }
 
         double height = Double.parseDouble(heightStr) / 100; // Chuyển cm thành mét
         double weight = Double.parseDouble(weightStr);
-
         double bmi = weight / (height * height);
         tvResult.setText(String.format("%.1f", bmi));
 
+        String message = getString(bmi);
+
+        tvStatus.setText(message);
+    }
+
+    @NonNull
+    private static String getString(double bmi) {
+        String message;
         if (bmi < 18.5) {
-            tvStatus.setText("Underweight");
+            message = "Bạn đang khá gầy, cần bổ sung thêm chất dinh dưỡng để cải thiện sức khỏe!";
         } else if (bmi >= 18.5 && bmi < 24.9) {
-            tvStatus.setText("Normal");
+            message = "Sức khỏe của bạn rất tốt! Hãy tiếp tục phát huy chế độ ăn uống và tập luyện nhé!";
         } else if (bmi >= 25 && bmi < 29.9) {
-            tvStatus.setText("Overweight");
+            message = "Bạn đang hơi thừa cân, hãy tập thể dục thường xuyên để duy trì sức khỏe!";
         } else {
-            tvStatus.setText("Obese");
+            message = "Bạn đang khá nặng, hãy giảm cân để bảo vệ sức khỏe của mình nhé!";
         }
+        return message;
     }
 }
